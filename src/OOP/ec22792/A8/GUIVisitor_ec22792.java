e43d33f46fcffa55eab6cf0f32ae7bdaa4c6ebda
ec22792 <ec22792@qmul.ac.uk>
2023-04-07 12:39:21 +0100

    Added files

--- src/OOP/ec22792/A8/GUIVisitor_ec22792.java
+++ src/OOP/ec22792/A8/GUIVisitor_ec22792.java
@@ -1,2 +1,38 @@
-package OOP.ec22792.A8;public class GUIVisitor_ec22792 {
+package OOP.ec22792.A8;
+
+public class GUIVisitor_ec22792 implements Visitor{
+    @Override
+    public void tell(String messageForVisitor) {
+
+    }
+
+    @Override
+    public char getChoice(String descriptioOfChoices, char[] arrayOfPossibleChoices) {
+        return 0;
+    }
+
+    @Override
+    public boolean giveItem(Item itemGivenToVisitor) {
+        return false;
+    }
+
+    @Override
+    public boolean hasIdenticalItem(Item itemToCheckFor) {
+        return false;
+    }
+
+    @Override
+    public boolean hasEqualItem(Item itemToCheckFor) {
+        return false;
+    }
+
+    @Override
+    public void giveGold(int numberOfPiecesToGive) {
+
+    }
+
+    @Override
+    public int takeGold(int numberOfPiecesToTake) {
+        return 0;
+    }
 }
--- src/OOP/ec22792/A8/Main.java
+++ src/OOP/ec22792/A8/Main.java
@@ -1,3 +1,5 @@
+package OOP.ec22792.A8;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
