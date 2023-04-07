4ebd35eb36ac26cb5b7faa4b30c5dab6c32b09c3
ec22792 <ec22792@qmul.ac.uk>
2023-04-07 12:57:28 +0100

    Started GUI and imported correct room

--- src/OOP/ec22792/A8/GUIVisitor_ec22792.java
+++ src/OOP/ec22792/A8/GUIVisitor_ec22792.java
@@ -1,13 +1,39 @@
 package OOP.ec22792.A8;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GUIVisitor_ec22792 implements Visitor{
+    public JFrame f= new JFrame();
+    public JPanel p;
+    public JLabel gold;
+    public int goldTot= 0;
+    public JLabel items;
+    public List<Item> itemList= new ArrayList<Item>();
+
+
+    //Constructor
+    public GUIVisitor_ec22792(JFrame f){
+        //Setting frame to size of phone screen
+        f.setSize(new Dimension(720,1280));
+        p= new JPanel();
+        gold= new JLabel("You have " + goldTot +" gold");
+        items= new JLabel("You have X items");
+
+        p.add(gold);
+        p.add(items);
+        f.add(p);
+
+    }
     @Override
     public void tell(String messageForVisitor) {
-
+        JOptionPane.showMessageDialog(null, messageForVisitor);
     }
 
     @Override
-    public char getChoice(String descriptioOfChoices, char[] arrayOfPossibleChoices) {
+    public char getChoice(String descriptionOfChoices, char[] arrayOfPossibleChoices) {
         return 0;
     }
 
--- src/OOP/ec22792/A8/Main.java
+++ src/OOP/ec22792/A8/Main.java
@@ -1,7 +1,10 @@
 package OOP.ec22792.A8;
 
+import javax.swing.*;
+
 public class Main {
+    public static JFrame f= new JFrame();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GUIVisitor_ec22792 g= new GUIVisitor_ec22792(f);
     }
 }
